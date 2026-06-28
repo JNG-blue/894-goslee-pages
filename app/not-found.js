0cbbc00e8b1c1a5086073c9efe4c1ec92f132054
J. G <luxlunae@gmail.com>
2026-06-28 23:40:37 +0000

    Adding not found and error handling pages #60

--- app/not-found.js
+++ app/not-found.js
@@ -2,15 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className={styles.notFound}>
+    <main>
       <h1>404</h1>
 
       <h2>Page Not Found</h2>
 
       <p>The page you're looking for doesn't exist or may have been moved.</p>
 
-      <div className={styles.links}>
-        <Link href="/">Home</Link>
+      <div>
         <Link href="/browse">Browse Books</Link>
       </div>
     </main>
