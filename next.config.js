172d41f69e9f452ec63c1dc0e20e2d0d5b747784
J. Goslee <luxlunae@gmail.com>
2026-07-23 19:23:55 +0000

    fix cors error #129 by updating next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000",
+        "fuzzy-yodel-jjr5gx5496g35qxp-3000.app.github.dev",
+        "*.app.github.dev",
+      ],
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
